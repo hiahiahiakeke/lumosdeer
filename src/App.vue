1368e05239e7f2d9137d25215c84a6d8fac16499
刘柯 <liuke@wdit.com.cn>
2020-02-28 14:44:27 +0800

    shiyishi

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@
         </router-link>
         <router-link to="map">
           <span>关于本站</span>
+          <span>关于本站</span>
+          <span>关于本站</span>
         </router-link>
       </div>
     </div>
